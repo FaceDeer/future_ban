3c1f47aaf35b645a42b835a5fbe8ce202b6af2f2
FaceDeer <derksenmobile@gmail.com>
2019-12-14 11:34:07 -0700

    add documentation, clean up code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# future_ban
- A Minetest mod that adds a command to queue an offline player for banning when they next join
+A Minetest mod that adds a "future_ban" command to queue an offline player for banning when they next join. Players need to have the "ban" priviledge to use this command.
+
+The list of player names to be banned is stored in the "future_ban.lua" file in the world folder for a particular game. As soon as a player with a name on this list logs in they will be immediately banned. In this way the IP address of the player doesn't need to be known at the time the ban is issued.
+
+# Usage
+
+``/future_ban`` without a player name will present a list of the names currently stored for banning. These players have not yet been banned. As soon as a player with a matching name logs in they'll be banned and removed from the list.
+
+``/future_ban <name>`` Adds a player name to the ban list, or removes it if it's already on the list.
+
+# Licensing
+
+This mod was originally created by cactuz_pl in 2012 and published under the WTFPL. It was rehosted and relicensed to MIT licensing by FaceDeer in 2019.
--- init.lua
+++ init.lua
@@ -1,63 +1,80 @@
 future_ban_list = {}
 
-local file = io.open(minetest.get_worldpath().."/future_banlist.txt", "r")
+local file = io.open(minetest.get_worldpath().."/future_banlist.lua", "r")
 if file then
-    future_ban_list = minetest.deserialize(file:read("*all"))
-    file:close()
-    if not future_ban_list then
-        future_ban_list = {}
-    end
+	future_ban_list = minetest.deserialize(file:read("*all"))
+	file:close()
+	if not future_ban_list then
+		future_ban_list = {}
+	end
 end
 
 local function save_file()
-    local file = io.open(minetest.get_worldpath().."/future_banlist.txt", "w")
-    if file then
-        file:write(minetest.serialize(future_ban_list))
-        file:close()
-    end
+	local file = io.open(minetest.get_worldpath().."/future_banlist.lua", "w")
+	if file then
+		file:write(minetest.serialize(future_ban_list))
+		file:close()
+	end
 end
 
 minetest.register_chatcommand("future_ban", {
-    params = "<playername> | omit playername to see the future ban list",
-    description = "The player will be banned when trying to join",
-    privs = {ban=true},
-    func = function(name, param)
-        if param == "" then
-            minetest.chat_send_player(name, "Future ban list: " .. dump(future_ban_list))
-            return
-        end
-        if not minetest.env:get_player_by_name(param) then
-            table.insert(future_ban_list, param)
-            minetest.chat_send_player(name, param .. " added to future ban list.")
-            minetest.log("action", name .. " added " .. param .. " to future ban list.")
-            save_file()
-            return
-        end
-        if not minetest.ban_player(param) then
-            table.insert(future_ban_list, param)
-            minetest.chat_send_player(name, desc .. " to future ban list added.")
-            minetest.log("action", name .. " added " .. desc .. " to future ban list.")
-            save_file()
-        else
-            local desc = minetest.get_ban_description(param)
-            minetest.chat_send_player(name, "Banned " .. desc .. ".")
-            minetest.log("action", name .. " bans " .. desc .. ".")
-        end
-    end
+	params = "<playername> | omit playername to see the future ban list",
+	description = "The player will be banned when trying to join",
+	privs = {ban=true},
+	func = function(name, param)
+		if param == "" then
+			local list = {}
+			local n=0
+			for k,v in pairs(future_ban_list) do
+				n = n+1
+				list[n] = k
+			end
+			if n == 0 then
+				minetest.chat_send_player(name, "Future ban list is empty.")
+			else
+				minetest.chat_send_player(name, "Future ban list:\n" .. table.concat(list, "\n"))
+			end
+			return
+		end
+		if minetest.get_ban_description(param) ~= "" then
+			local desc = minetest.get_ban_description(param)
+			minetest.chat_send_player(name, param .. " has been previously banned with the IP numbers " .. desc .. ".")
+		end
+		if future_ban_list[param] then
+			future_ban_list[param] = nil
+			minetest.chat_send_player(name, param .. " removed from the future ban list.")
+			return
+		end
+		if not minetest.get_player_by_name(param) then
+			future_ban_list[param] = true
+			minetest.chat_send_player(name, param .. " added to the future ban list.")
+			minetest.log("action", name .. " added " .. param .. " to the future ban list.")
+			save_file()
+			return
+		end
+		if not minetest.ban_player(param) then
+			future_ban_list[param] = true
+			minetest.chat_send_player(name, param .. " added to the future ban list.")
+			minetest.log("action", name .. " added " .. param .. " to the future ban list.")
+			save_file()
+		else
+			local desc = minetest.get_ban_description(param)
+			minetest.chat_send_player(name, "Banned " .. desc .. ".")
+			minetest.log("action", name .. " bans " .. desc .. ".")
+		end
+	end
 })
 
 minetest.register_on_joinplayer(function(player)
-    local name = player:get_player_name()
-    for i,n in ipairs(future_ban_list) do
-        if n == name then
-            if not minetest.ban_player(name) then
-                minetest.chat_send_player(name, "Failed to ban player " .. name .. " (from future ban list).")
-            else
-                local desc = minetest.get_ban_description(name)
-                minetest.log("action", desc .. " banned (from future ban list).")
-                table.remove(future_ban_list, i)
-                save_file()
-            end
-        end
-    end
+	local name = player:get_player_name()
+	if future_ban_list[name] then
+		if not minetest.ban_player(name) then
+			minetest.log("warning", "Failed to ban player " .. name .. " who was on the future ban list when he joined.")
+		else
+			local desc = minetest.get_ban_description(name)
+			minetest.log("action", desc .. " banned (from future ban list).")
+			future_ban_list[name] = nil
+			save_file()
+		end
+	end
 end)
